7c22de59efe84807fc16a5241f724a98deaaaf3c
Tejas H <tejas007tez@gmail.com>
2021-01-13 15:08:34 +0530

    Completed

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-return-await */
 function allPromises(listOfPromises) {
+  // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     const results = [];
 
     const promisesWrappedInResolve = listOfPromises.map(Promise.resolve, Promise);
     for (const promise of promisesWrappedInResolve) {
+      // eslint-disable-next-line no-await-in-loop
       results.push(await promise.then(async resolvedData => await resolvedData, reject));
       if (results.length === listOfPromises.length) resolve(results);
     }
