795dc0e5891371906dd345d0d87e55d6b418f1a9
Tejas H <tejas007tez@gmail.com>
2021-01-18 22:48:53 +0530

    Not completed yet

---
+++ js-exercises/sum-primes/sumPrimes.js
@@ -0,0 +1,40 @@
+function isPrime(num) {
+  if (num < 2) return false;
+
+  for (let k = 2; k < num; k += 1) {
+    if (num % k === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumPrimes(upTo) {
+  return {
+    current: 2,
+    [Symbol.iterator]() {
+      this.upTo = upTo;
+      return this;
+    },
+
+    next() {
+      for (let i = 2; i < this.upTo; i += 1) {
+        if (isPrime(i)) {
+          return {
+            done: false,
+            value: i,
+          };
+        }
+      }
+      return { done: true };
+    },
+  };
+}
+
+// function primeGenerator() {
+
+// }
+
+console.log([...sumPrimes(10)]);
+
+export { sumPrimes };
